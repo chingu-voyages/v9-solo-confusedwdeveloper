a93818a695b56a54da5540bcc661bb6100762939
Darshan Datta <darshandatta.10@gmail.com>
2019-06-22 14:17:15 +0530

    create recipes functionality added, getRecipes async function created

--- src/ui.js
+++ src/ui.js
@@ -4,6 +4,8 @@ import * as firebase from "firebase/app";
 // Add the Firebase products that you want to use
 import "firebase/auth";
 import "firebase/firestore";
+import uuidv4 from "uuid/v4";
+import moment from "moment";
 
 // set up modal Body
 const setUpModalBody = user => {
@@ -11,11 +13,31 @@ const setUpModalBody = user => {
   const para1 = document.createElement("p");
   const para2 = document.createElement("p");
   para1.textContent = `Name:  ${user.displayName}`;
-  para2.textContent = `Email:  ${user.email}`;
+  if (user.email) {
+    para2.textContent = `Email:  ${user.email}`;
+  }
   div.appendChild(para1);
   div.appendChild(para2);
   return div;
 };
+// function to create a new recipe
+const getRecipes = async user => {
+  let recipes;
+  const doc = await firebase
+    .firestore()
+    .collection("users")
+    .doc(user.uid)
+    .get();
+
+  if (doc.exists) {
+    recipes = doc.data().recipes;
+  } else {
+    recipes = [];
+  }
+
+  return recipes;
+};
+
 // function to set up navbar
 const setUpNavbar = user => {
   const logout = document.querySelector("#logout");
@@ -33,5 +55,33 @@ const setUpNavbar = user => {
       });
   });
   modalBody.appendChild(setUpModalBody(user));
+
+  // set up create recipe button
+  const createRecipeButton = document.querySelector("#go-to-update");
+  createRecipeButton.addEventListener("click", e => {
+    const id = uuidv4();
+    const time = moment().valueOf();
+    getRecipes(user)
+      .then(recipes => {
+        recipes.push({
+          id,
+          createdAt: time,
+          updatedAt: time,
+          title: "",
+          body: "",
+          ingredients: []
+        });
+        return firebase
+          .firestore()
+          .collection("users")
+          .doc(user.uid)
+          .set({
+            recipes: recipes
+          });
+      })
+      .then(() => {
+        location.assign(`/update.html#${id}`);
+      });
+  });
 };
 export { setUpNavbar };
