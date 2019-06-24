8e1bf8adf3a5dc171cbb758e56b31bc59d77e4f6
Darshan Datta <darshandatta.10@gmail.com>
2019-06-24 21:50:10 +0530

    fixed some bugs

--- src/setup-update.js
+++ src/setup-update.js
@@ -4,77 +4,7 @@ import * as firebase from "firebase/app";
 // Add the Firebase products that you want to use
 import "firebase/auth";
 import "firebase/firestore";
-import moment from "moment";
 import { getRecipes } from "./ui";
-import uuidv4 from "uuid/v4";
-
-// function to setup update form
-const setupUpdateForm = async (user, recipeId) => {
-  const recipes = await getRecipes(user);
-  const recipe = recipes.find(i => i.id === recipeId);
-  // if (!recipe) {
-  //   location.assign("/recipes.html");
-  // }
-  const updateForm = document.querySelector("#update-form");
-  updateForm.addEventListener("submit", e => {
-    e.preventDefault();
-    const title = e.target.elements.title.value.trim();
-    const body = e.target.elements.body.value.trim();
-    recipe.title = title;
-    recipe.body = body;
-    recipe.updatedAt = moment().valueOf();
-    firebase
-      .firestore()
-      .collection("users")
-      .doc(user.uid)
-      .set({
-        recipes: recipes
-      });
-    e.target.elements.title.value = "";
-    e.target.elements.body.value = "";
-    // renderUpdate(user, recipeId);
-  });
-};
-
-// function to set up add ingredient form
-const addIngredientForm = async (user, recipeId) => {
-  const form = document.querySelector("#add-ingredient-form");
-
-  // getting recipes
-  const recipes = await getRecipes(user);
-  const recipe = recipes.find(i => i.id === recipeId);
-  // if (!recipe) {
-  //   location.assign("/recipes.html");
-  // }
-  const ingredients = recipe.ingredients;
-  form.addEventListener("submit", e => {
-    e.preventDefault();
-    const text = e.target.elements.newIngredient.value.trim();
-    const id = uuidv4();
-    if (text) {
-      const ingredient = {
-        id,
-        text,
-        completed: false
-      };
-      //adding to array
-      ingredients.push(ingredient);
-
-      // updating timestamp
-      recipe.updatedAt = moment().valueOf();
-
-      //saving to database
-      firebase
-        .firestore()
-        .collection("users")
-        .doc(user.uid)
-        .set({
-          recipes: recipes
-        });
-      form.reset();
-    }
-  });
-};
 
 // function to remove one recipe
 const removeRecipe = async (user, recipeId) => {
--- src/update.js
+++ src/update.js
@@ -3,14 +3,11 @@ import "./scss/recipes.scss";
 // Firebase App (the core Firebase SDK) is always required and must be listed first
 import * as firebase from "firebase/app";
 import { setUpNavbar } from "./ui";
-import {
-  setupUpdateForm,
-  addIngredientForm,
-  removeRecipe
-} from "./setup-update";
+import { removeRecipe } from "./setup-update";
 import { renderUpdate } from "./update-view";
 import { setFilters } from "./filters";
-
+import moment from "moment";
+import uuidv4 from "uuid/v4";
 // Add the Firebase products that you want to use
 import "firebase/auth";
 import "firebase/firestore";
@@ -23,6 +20,8 @@ const textFilterIngredients = document.querySelector(
   "#text-filter-ingredients"
 );
 const checkboxFilter = document.querySelector("#hide-completed");
+const form = document.querySelector("#add-ingredient-form");
+const updateForm = document.querySelector("#update-form");
 
 const recipeId = location.hash.substring(1);
 
@@ -41,6 +40,7 @@ firebase.auth().onAuthStateChanged(function(user) {
       .doc(user.uid)
       .onSnapshot(doc => {
         const recipes = doc.data().recipes;
+        const recipe = recipes.find(i => i.id === recipeId);
         renderUpdate(recipes, recipeId, user);
 
         // event listener on ingredient search filter
@@ -60,10 +60,53 @@ firebase.auth().onAuthStateChanged(function(user) {
           });
           renderUpdate(recipes, recipeId, user);
         });
-      });
 
-    setupUpdateForm(user, recipeId);
-    addIngredientForm(user, recipeId);
+        updateForm.addEventListener("submit", e => {
+          e.preventDefault();
+          const title = e.target.elements.title.value.trim();
+          const body = e.target.elements.body.value.trim();
+          recipe.title = title;
+          recipe.body = body;
+          recipe.updatedAt = moment().valueOf();
+          firebase
+            .firestore()
+            .collection("users")
+            .doc(user.uid)
+            .set({
+              recipes: recipes
+            });
+        });
+        form.addEventListener("submit", e => {
+          e.preventDefault();
+          const ingredients = recipe.ingredients;
+          const text = e.target.elements.newIngredient.value.trim();
+          const id = uuidv4();
+          if (text) {
+            const ingredient = {
+              id,
+              text,
+              completed: false
+            };
+            //adding to array
+            ingredients.push(ingredient);
+
+            // updating timestamp
+            recipe.updatedAt = moment().valueOf();
+
+            //saving to database
+            firebase
+              .firestore()
+              .collection("users")
+              .doc(user.uid)
+              .set({
+                recipes: recipes
+              })
+              .then(() => {
+                form.reset();
+              });
+          }
+        });
+      });
 
     const removeButton = document.querySelector("#remove-recipe");
     // setting up event listener on remove Button
